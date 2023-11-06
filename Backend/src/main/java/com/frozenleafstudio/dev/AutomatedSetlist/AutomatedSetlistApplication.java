651303fb256a9f4164fdb4cdc54c5cb3e87831f5
Stephen Rooks <stephenp.rooks@hotmail.com>
2023-11-06 17:20:47 -0500

    Spring Boot initialization
    Adding java Spring Boot content to backend folder. Added spring-dotenv gradle dependency and moved mongoDB credentials to .ENV file. Successful DB connection using env variables.

---
+++ Backend/src/main/java/com/frozenleafstudio/dev/AutomatedSetlist/AutomatedSetlistApplication.java
@@ -0,0 +1,14 @@
+package com.frozenleafstudio.dev.AutomatedSetlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedSetlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedSetlistApplication.class, args);
+	}
+	
+
+}
---
+++ Backend/src/test/java/com/frozenleafstudio/dev/AutomatedSetlist/AutomatedSetlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frozenleafstudio.dev.AutomatedSetlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedSetlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
