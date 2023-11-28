f92342cb18bd7534f3cb9837a885cf518a204bbd
Stephen Rooks <stephenp.rooks@hotmail.com>
2023-11-28 14:27:59 -0500

    beginning work on frontend

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import ProjectsPage from "./projects/ProjectsPage";
+
+function App() {
+  return (
+    <div className="container">
+      <ProjectsPage />
+    </div>
+  );
+}
+
+export default App;
