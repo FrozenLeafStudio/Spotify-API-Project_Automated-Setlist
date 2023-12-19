b8505c1193ad8c7adac5011e420b3c553c20d5b7
Stephen Rooks <stephenp.rooks@hotmail.com>
2023-12-19 14:38:09 -0500

    frontend model and adjust mongodb index

--- Backend/src/main/java/com/frozenleafstudio/dev/AutomatedSetlist/Playlist/Playlist.java
+++ Backend/src/main/java/com/frozenleafstudio/dev/AutomatedSetlist/Playlist/Playlist.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Document(collection = "playlists")
+@Document(collection = "Playlists")
 public class Playlist {
 
     @Id
--- Frontend/src/models/Setlist.ts
+++ Frontend/src/models/Setlist.ts
@@ -1,13 +1,57 @@
 export class Setlist {
   setlistID: string = "";
   eventDate: string = "";
-  mbid: string = ""; // Matches an artist in the Artists Collection
-  venueName: string = "";
-  venueLocation: string = "";
+  artist: {
+    mbid: string;
+    name: string;
+    url: string;
+  } = { mbid: "", name: "", url: "" }; // Matches an artist in the Artists Collection
+  venue: {
+    name: string;
+    city: {
+      name: string;
+      state: string;
+      stateCode: string;
+      country: {
+        code: string;
+        name: string;
+      };
+    };
+    url: string;
+  } = {
+    name: "",
+    city: {
+      name: "",
+      state: "",
+      stateCode: "",
+      country: { code: "", name: "" },
+    },
+    url: "",
+  };
   tourName: string = "";
+  sets: {
+    set: {
+      name: string;
+      song: {
+        name: string;
+        info: string;
+        tape: boolean;
+        cover: {
+          name: string;
+          mbid: string;
+          sortName: string;
+          url: string;
+        } | null;
+        with: {
+          mbid: string;
+          name: string;
+          url: string;
+        } | null;
+      }[];
+    }[];
+    url: string;
+  } = { set: [], url: "" };
   url: string = "";
-  songs: string[] = []; // List of song names
-
   constructor(initializer: Partial<Setlist> = {}) {
     Object.assign(this, initializer);
   }
