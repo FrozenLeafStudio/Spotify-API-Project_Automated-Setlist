a091d1034eee0c5db8bd6a9fee9f719a77956771
Stephen Rooks <stephenp.rooks@hotmail.com>
2024-01-04 14:25:43 -0500

    async spotify track search

--- Backend/src/main/java/com/frozenleafstudio/dev/AutomatedSetlist/AutomatedSetlistApplication.java
+++ Backend/src/main/java/com/frozenleafstudio/dev/AutomatedSetlist/AutomatedSetlistApplication.java
@@ -1,14 +1,31 @@
 package com.frozenleafstudio.dev.AutomatedSetlist;
 
+import java.util.concurrent.Executor;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class AutomatedSetlistApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AutomatedSetlistApplication.class, args);
+	}
+
+	@Bean
+  	public Executor taskExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(2);
+		executor.setMaxPoolSize(2);
+		executor.setQueueCapacity(100);
+		executor.setThreadNamePrefix("Playlist-");
+		executor.initialize();
+		return executor;
 	}
 }
