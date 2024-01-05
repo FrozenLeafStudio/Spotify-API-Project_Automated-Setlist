1c83aba890e24a6cb20cff7bb613e6c313e42deb
Stephen Rooks <stephenp.rooks@hotmail.com>
2024-01-05 15:39:43 -0500

    Update Loading.tsx

--- Frontend/src/features/style/Loading.tsx
+++ Frontend/src/features/style/Loading.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { FcProcess } from "react-icons/fc";
 import { animated, useSpring } from "react-spring";
 import "./Loading.css";
