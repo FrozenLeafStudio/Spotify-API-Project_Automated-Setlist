6c5c6964d52bb0853e388cf9dbd758d230dff23d
Stephen Rooks <stephenp.rooks@hotmail.com>
2024-01-05 16:03:08 -0500

    Update Loading.tsx

--- Frontend/src/features/style/Loading.tsx
+++ Frontend/src/features/style/Loading.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FcProcess } from "react-icons/fc";
 import { useBoop } from "./useBoop";
 import { animated } from "react-spring";
@@ -7,17 +7,18 @@ import "./Loading.css";
 export const Loading: React.FC = () => {
   const [style, trigger] = useBoop({
     rotation: 360,
-    timing: 4500,
+    timing: 4000,
     springConfig: {
-      tension: 180,
-      friction: 12,
+      tension: 100,
+      friction: 20,
     },
   });
 
-  React.useEffect(() => {
+  useEffect(() => {
     trigger();
-    const intervalId = setInterval(trigger, 4500);
-    return () => clearInterval(intervalId);
+    const interval = setInterval(trigger, 4000);
+
+    return () => clearInterval(interval); // Cleanup the interval when component unmounts
   }, [trigger]);
 
   return (
