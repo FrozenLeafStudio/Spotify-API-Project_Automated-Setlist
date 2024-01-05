854f7fdb9532c73bc47b445e52d1875ddf5f85bc
Stephen Rooks <stephenp.rooks@hotmail.com>
2024-01-05 14:46:01 -0500

    center & blur

--- Frontend/src/features/style/Loading.tsx
+++ Frontend/src/features/style/Loading.tsx
@@ -2,26 +2,28 @@ import React from "react";
 import { FcProcess } from "react-icons/fc";
 import { useBoop } from "./useBoop";
 import { animated } from "react-spring";
-import "/Loading.css";
+import "./Loading.css";
 
 export const Loading: React.FC = () => {
   const [style, trigger] = useBoop({
-    rotation: 90,
-    timing: 1000,
+    rotation: 360,
+    timing: 1500,
     springConfig: {
-      tension: 200,
-      friction: 10,
+      tension: 180,
+      friction: 12,
     },
   });
 
   React.useEffect(() => {
-    const intervalId = setInterval(trigger, 1000);
+    const intervalId = setInterval(trigger, 1500);
     return () => clearInterval(intervalId);
   }, [trigger]);
 
   return (
-    <animated.div style={style} className="loading-overlay">
-      <FcProcess size={50} className="loading-icon" />
-    </animated.div>
+    <div className="loading-overlay">
+      <animated.div style={style}>
+        <FcProcess size={50} className="loading-icon" />
+      </animated.div>
+    </div>
   );
 };
