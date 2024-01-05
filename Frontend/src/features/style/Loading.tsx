34d6346e70a8e3ad171942a8227f00632d0bddbe
Stephen Rooks <stephenp.rooks@hotmail.com>
2024-01-05 15:41:15 -0500

    Update Loading.tsx

--- Frontend/src/features/style/Loading.tsx
+++ Frontend/src/features/style/Loading.tsx
@@ -6,7 +6,7 @@ import "./Loading.css";
 export const Loading: React.FC = () => {
   const rotatingStyle = useSpring({
     from: { rotateZ: 0 },
-    to: { rotateZ: 360 },
+    to: { rotateZ: -360 },
     config: { duration: 6000 }, // Duration for a full 360-degree rotation
     loop: true, // Loop the animation indefinitely
     reset: true, // Reset the animation on each iteration to smoothly transition
